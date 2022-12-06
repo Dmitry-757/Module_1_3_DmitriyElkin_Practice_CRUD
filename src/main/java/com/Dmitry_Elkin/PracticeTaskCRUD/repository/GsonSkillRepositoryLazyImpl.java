5b5b37b131889d146eda5bf5e5abd09235dd8042
Dmitry-757 <glv11.glv11.glv11@gmail.com>
2022-12-06 17:11:45 +0300

    some meths from service were moves to repositoryLazy

--- src/main/java/com/Dmitry_Elkin/PracticeTaskCRUD/repository/GsonSkillRepositoryLazyImpl.java
+++ src/main/java/com/Dmitry_Elkin/PracticeTaskCRUD/repository/GsonSkillRepositoryLazyImpl.java
@@ -2,21 +2,60 @@ package com.Dmitry_Elkin.PracticeTaskCRUD.repository;
 
 import com.Dmitry_Elkin.PracticeTaskCRUD.model.Skill;
 import com.Dmitry_Elkin.PracticeTaskCRUD.service.SkillService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 
 public class GsonSkillRepositoryLazyImpl implements SkillRepository{
-
     public HashMap<Long, Skill> getSkillSet() {
         return skillSet;
     }
 
     private final HashMap<Long, Skill> skillSet;
 
+    private static final Path file = Paths.get("skillsHM.json");
+    private static final Gson gson = new GsonBuilder()
+            .setPrettyPrinting() //formats json-file to well done form
+            .create();
+
     //при старте считываем данные из бд в skillSet что бы в дальнейшем дергать данные из skillSet, а не обращаться к бд каждый раз
     public GsonSkillRepositoryLazyImpl() {
-        skillSet = SkillService.getItemsFromDB();
+        skillSet = getItemsFromDB();
     }
+
+    public static HashMap<Long, Skill> getItemsFromDB(){
+        if (Files.exists(file)) {
+            String jsonStr;
+            try {
+                jsonStr = Files.readString(file);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Type targetClassType = new TypeToken<HashMap<Long, Skill>>() {
+            }.getType();
+            return new Gson().fromJson(jsonStr, targetClassType);
+        };
+        return new HashMap<Long, Skill>();
+
+    }
+
+    public static void updateBD(HashMap<Long, Skill> skillSet) {
+        try {
+            Files.writeString(file, gson.toJson(skillSet));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 
     @Override
     public List<Skill> getAll() {
@@ -34,13 +73,14 @@ public class GsonSkillRepositoryLazyImpl implements SkillRepository{
             item.setNewId();
         }
         skillSet.put(item.getId(), item);
-        SkillService.updateBD(skillSet);
+        updateBD(skillSet);
         return true;
     }
 
     @Override
     public boolean delete(Skill item) {
         item.setDeleted();
-        return false;
+        addOrUpdate(item);
+        return true;
     }
 }
